423782b9c3de85727fc20d87c04a00ef76d93a6c
Sweta-Swarnakar <68773277+Sweta-Swarnakar@users.noreply.github.com>
2026-04-16 02:37:28 +0530

    Revert "updated depricated package"
    This reverts commit 5d3ba5d4bf98cad3e4116fdfc17d2673a418a85c.

--- src/components/layouts/Particles.js
+++ src/components/layouts/Particles.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Particles from "react-tsparticles";
+import Particles from 'react-particles-js'
 import { background } from '../../profile'
 
 const ParticlesBackground = () => {
